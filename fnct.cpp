4169c191a9d9ffe9cb5a5ed96b578d0ae606eff7
sakshi0423 <sakshidhar44@gmail.com>
2024-07-30 15:19:24 +0530

    Initial Commit

---
+++ fnct.cpp
@@ -0,0 +1,19 @@
+#include "fnct.hpp"
+
+void bleep(std::string &text, std::string word) {
+  
+  for (int i = 0; i < text.size(); i++) {
+
+    int check = 0;
+
+    for (int j = 0; j < word.size(); j++) {
+      if (text[i+j] == word[j]) { check++; }
+    }
+
+    if (check == word.size()) {
+      for (int k = 0; k < word.size(); k++) { 
+        text[i+k] = '*'; 
+      }
+    }
+  }
+}
