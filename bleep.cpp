075679f8ec2d31b7a1ff2773a1e5579c575648bb
sakshi0423 <sakshidhar44@gmail.com>
2024-07-31 11:19:01 +0530

    Initial Commit

---
+++ bleep.cpp
@@ -0,0 +1,14 @@
+#include "fnct.hpp"
+
+int main() {
+
+  std::string word = "broccoli";
+  std::string text = "I'm smoking on that broccoli. That Evil Pack broccoli kush. That Real Hitter broccoli.";
+
+  bleep(text, word);
+  
+  std::cout << text << "\n";
+  std::cin.get();
+  
+  return 0;
+}
